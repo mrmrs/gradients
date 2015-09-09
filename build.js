3ccceb33731f472503c52dc74313bcf85f4ecf26
mrmrs <hi@mrmrs.cc>
2015-09-09 18:16:36 +0800

    First stab at build process for css compilation using postcss

---
+++ build.js
@@ -0,0 +1,34 @@
+// dependencies
+
+var fs = require("fs")
+var autoprefixer = require("autoprefixer")
+var postcss = require("postcss")
+var atImport = require("postcss-import")
+var cssvariables = require('postcss-css-variables')
+var compressor = require('node-minify')
+var conditionals = require('postcss-conditionals')
+var customMedia = require("postcss-custom-media")
+
+// css to be processed
+var css = fs.readFileSync("src/tachyons-background-size.css", "utf8")
+
+// process css
+var output = postcss([autoprefixer])
+  .use(atImport())
+  .use(cssvariables())
+  .use(conditionals())
+  .use(customMedia())
+  .process(css, {
+    from: "src/gradients.css",
+    to: "gradients.css"
+  })
+  .css
+
+fs.writeFile("gradients.css", output, 'utf-8')
+
+// Using YUI Compressor for CSS
+new compressor.minify({
+    type: 'sqwish',
+    fileIn: 'gradients.css',
+    fileOut: 'gradients.min.css',
+});
